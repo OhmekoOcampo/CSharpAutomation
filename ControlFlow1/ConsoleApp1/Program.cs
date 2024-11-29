f5f53205be2dc8f15d807b3124b5b3836280f5b9
Ohmeko Ocampo <ohmeko.ocampo@gmail.com>
2024-11-28 22:00:32 -0800

    Completed Control Flow Exercises 43. Finished Speed Camera method and Image Orientation Method.

--- ControlFlow1/ConsoleApp1/Program.cs
+++ ControlFlow1/ConsoleApp1/Program.cs
@@ -8,6 +8,11 @@ using System.Xml.Schema;
 
 namespace ControlFlowOne
 {
+    public enum ImageOrientation
+    {
+        Landscape,
+        Portrait
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -15,8 +20,8 @@ namespace ControlFlowOne
             Console.WriteLine("Please enter a number between 1 to 10:");
 
             int x = Int32.Parse(Console.ReadLine());
-            string Continue = "Yes";
             
+
             if (x >= 1 && x <= 10)
             {
                 Console.WriteLine("Number is Valid!");
@@ -34,13 +39,55 @@ namespace ControlFlowOne
             if (numbah1 > numbah2)
             {
                 Console.WriteLine("The first number {0} is the bigger number", numbah1);
-            }else if(numbah2 > numbah1)
+            }
+            else if (numbah2 > numbah1)
             {
                 Console.WriteLine("The second number {0} is the bigger number", numbah2);
             }
             else
             {
                 Console.WriteLine("Both the first and and second number are the same!");
+            }
+            Orientation(300, 400);
+            SpeedCamera();
+
+        }
+
+        public static void Orientation(int Width, int Height)
+        {
+            if (Width > Height)
+            {
+                Console.WriteLine("Image is in {0} Mode!", ImageOrientation.Landscape);
+            }
+            else
+            {
+                Console.WriteLine("Image is in {0} Mode!", ImageOrientation.Portrait);
+            }
+        }
+
+        public static void SpeedCamera()
+        {
+            Console.WriteLine("Please Enter SpeedLimit: ");
+            int SpeedLimit = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What is the speed of the vehicle?");
+            int VehicleSpeed = Int32.Parse(Console.ReadLine());
+            if (VehicleSpeed <= SpeedLimit)
+            {
+                Console.WriteLine("Okay!");
+            }
+            else
+            {
+                int AboveSpeed = VehicleSpeed - SpeedLimit;
+                int DemeritPoints = AboveSpeed / 5;
+                if (DemeritPoints > 12)
+                {
+                    Console.WriteLine("License is Revoked! Take the Driving Test Again!");
+                }
+                else 
+                {
+                    Console.WriteLine("Watch your speed!");
+                    Console.WriteLine("Total Demerit Points: {0}", DemeritPoints);
+                }
             }
         }
     }
